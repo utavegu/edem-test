ee5b98ccbde84ef2e25126ebea55f54f2c2e3c03
Utavegu <utavegu@list.ru>
2023-11-23 17:55:38 +0500

    feat: add fetch trips logic on server

--- server/src/modules/trips/trips.controller.ts
+++ server/src/modules/trips/trips.controller.ts
@@ -1,22 +1,33 @@
-import { Controller, Post, Get } from '@nestjs/common';
+import { Controller, Post, Get, Query } from '@nestjs/common';
 import { TripsService } from './trips.service';
 import { Trip } from './schemas/trip.schema';
+import { Statuses } from './typespaces/enums/Statuses';
 
 @Controller('trips')
 export class TripsController {
   constructor(private readonly tripsService: TripsService) {}
 
   @Post()
-  copyTripsOnDb(): Promise<{ status: string; message: string }> {
+  copyTripsOnDb(): Promise<{
+    status: Statuses;
+    message: string;
+  }> {
     return this.tripsService.copyTripsOnDb();
   }
 
   @Get()
-  fetchTrips(): Promise<{
-    status: string;
+  fetchTrips(@Query() { limit, offset }): Promise<{
+    status: Statuses;
     data: Trip[];
+  }> {
+    return this.tripsService.fetchTrips(limit, offset);
+  }
+
+  @Get('amount')
+  getTripsAmount(): Promise<{
+    status: Statuses;
     amount: number;
   }> {
-    return this.tripsService.fetchTrips();
+    return this.tripsService.getTripsAmount();
   }
 }
--- server/src/modules/trips/trips.service.ts
+++ server/src/modules/trips/trips.service.ts
@@ -3,16 +3,17 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Trip, TripDocument } from './schemas/trip.schema';
 import { mockDatabase } from 'src/mockDatabase';
+import { Statuses } from './typespaces/enums/Statuses';
 
 @Injectable()
 export class TripsService {
   constructor(@InjectModel(Trip.name) private TripModel: Model<TripDocument>) {}
 
-  async copyTripsOnDb(): Promise<{ status: string; message: string }> {
+  async copyTripsOnDb(): Promise<{ status: Statuses; message: string }> {
     try {
       await this.TripModel.insertMany(mockDatabase.trips);
       return {
-        status: 'success',
+        status: Statuses.SUCCESS,
         message: 'Поездки добавлены успешно!',
       };
     } catch (err) {
@@ -20,17 +21,36 @@ export class TripsService {
     }
   }
 
-  async fetchTrips(): Promise<{
-    status: string;
+  async fetchTrips(
+    limit = '10',
+    offset = '0',
+  ): Promise<{
+    status: Statuses;
     data: Trip[];
+  }> {
+    try {
+      const trips = await this.TripModel.find()
+        .limit(Number(limit))
+        .skip(Number(offset))
+        .select('-__v');
+      return {
+        status: Statuses.SUCCESS,
+        data: trips,
+      };
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  async getTripsAmount(): Promise<{
+    status: Statuses;
     amount: number;
   }> {
     try {
-      const trips = await this.TripModel.find();
+      const tripsAmount = await this.TripModel.find().countDocuments();
       return {
-        status: 'success',
-        data: trips,
-        amount: trips.length, // временно!
+        status: Statuses.SUCCESS,
+        amount: tripsAmount,
       };
     } catch (err) {
       console.error(err);
